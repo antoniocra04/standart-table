56ea2019028a5095aa2703f79d07b303f6f00272
antoniocra04 <antoniocra04@gmail.com>
2022-02-03 16:13:08 +0600

    chore: setup enviroment

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
-import './App.css';
-
-function App() {
+export const App: React.FC = () => {
   return (
     <div className="App">
       <a></a>
     </div>
   );
-}
-
-export default App;
+};
